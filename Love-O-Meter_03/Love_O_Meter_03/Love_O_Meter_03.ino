f01585975414f356c27a7bb64d4fc53f8b3b0f6b
patelsagar23 <patelsagar23@gmail.com>
2014-08-08 10:57:52 -0500

    Tweaking
    Tweaked the code so the lights will turn on when temperature is changed
    one degree celsius.

--- Love-O-Meter_03/Love_O_Meter_03/Love_O_Meter_03.ino
+++ Love-O-Meter_03/Love_O_Meter_03/Love_O_Meter_03.ino
@@ -1,5 +1,6 @@
-const int ssensorPin =A0;
-const float baselineTemp = 20.0;
+const int sensorPin=A0;
+const float baselineTemp = 16.3;
+//Must tweak baselineTemp at start
 
 void setup(){
   Serial.begin(9600);
@@ -27,17 +28,17 @@ void loop(){
     digitalWrite(3,LOW);
     digitalWrite(4,LOW);
   
-  }else if(temperature >=baselineTemp+2 && temperature < baselineTemp+4){
+  }else if(temperature >=baselineTemp+1 && temperature < baselineTemp+2){
     digitalWrite(2,HIGH);
     digitalWrite(3,LOW);
     digitalWrite(4,LOW);
     
-  }else if(temperature >=baselineTemp+4 && temperature < baselineTemp+6){
+  }else if(temperature >=baselineTemp+2 && temperature < baselineTemp+3){
     digitalWrite(2,HIGH);
     digitalWrite(3,HIGH);
     digitalWrite(4,LOW);
     
-  }else if(temperature >=baselineTemp+6 && temperature < baselineTemp+8){
+  }else if(temperature >=baselineTemp+3 && temperature < baselineTemp+4){
     digitalWrite(2,HIGH);
     digitalWrite(3,HIGH);
     digitalWrite(4,HIGH);
